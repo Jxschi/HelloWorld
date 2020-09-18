53b839e8a78a533b91c726cb4db1af5d35787f05
JoschuaBuerkle <JoschuaBuerkle@0-208-08>
2020-09-18 12:53:25 +0200

    2

--- HelloWord/src/HelloWord.java
+++ HelloWord/src/HelloWord.java
@@ -1,9 +1,20 @@
 
 public class HelloWord {
+	
+	static int counter = 0;
+	
 
 	public static void main(String[] args) {
+		 
+		 while(counter<100) {
+	          if(counter%5==0) { 
+	        	  System.out.println("HelloWord!");}
+	          else {System.out.println("HelloWord"); }
 	     System.out.println("HelloWord!");
-
+		 System.out.println(counter);
+		 counter++;
+	     }
+		 }
 	}
 
-}
+
